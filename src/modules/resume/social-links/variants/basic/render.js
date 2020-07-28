3f5a0a4c7eb5c186fb930d86e13eef628d898538
Manoj <manoj.satishkumar@walmartlabs.com>
2020-07-28 08:32:20 +0530

    updated docs

--- src/modules/resume/social-links/variants/basic/render.js
+++ src/modules/resume/social-links/variants/basic/render.js
@@ -57,7 +57,7 @@ const Basic = (
                   ))
                 }
               </svg>
-              <a style={{ ...styles.anchor, color: theme.value.contrast }} href={link.value} target="_blank">{link.value}</a>
+              <a style={{ ...styles.anchor, color: theme.value.contrast }} href={link.value} rel="noreferrer" target="_blank">{link.value}</a>
             </div>
           })
         }
